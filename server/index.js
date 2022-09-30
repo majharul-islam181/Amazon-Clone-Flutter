8e434c50db99ea0edf2130f795d0e8120ad5e670
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-09-30 17:28:39 +0600

    node install

---
+++ server/index.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+//print('hello world')
+const express = require('express');
+//import 'package:express/express.dart'
+
+const PORT = 3000;
+
+const app = express(); 
+
+//Creating  An API
+app.listen(PORT, "0.0.0.0");
+
+
+
+
+
